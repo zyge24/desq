89dc50331c1a52fbed296da62aefa371a1e76ed8
zyge24 <71499666+zyge24@users.noreply.github.com>
2023-04-12 15:43:11 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'DesQ',
       theme: ThemeData(
-       // useMaterial3: true,
+        // useMaterial3: true,
 
         primaryColor: Colors.green,
         secondaryHeaderColor: Colors.green,
@@ -61,7 +61,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -81,84 +80,101 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
 
         leading: Icon(Icons.menu, size: 40),
-        actions: [Container( padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-          child:Icon(
-            Icons.search,
-            size: 40,
-            shadows: [Shadow(color: Colors.black, blurRadius: 15.0)],
-
-          ),)
+        actions: [
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+            child: Icon(
+              Icons.search,
+              size: 40,
+              shadows: [Shadow(color: Colors.black, blurRadius: 15.0)],
+            ),
+          )
         ],
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-        //bottom: Text("ASD"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ItemCard(
-                itemName: "Testi 1",
-                price: "300,00€",
-                image: 'graphics/photo1.jpg'),
-            ItemCard(
-                itemName: "Testi 2",
-                price: "200,00€",
-                image: 'graphics/photo2.jpg'),
-            ItemCard(
-                itemName: "Testi 3",
-                price: "300,00€",
-                image: 'graphics/photo3.jpg'),
-            ItemCard(
-                itemName: "Testi 4",
-                price: "200,00€",
-                image: 'graphics/photo4.jpg'),
-            ItemCard(
-                itemName: "Testi 5",
-                price: "300,00€",
-                image: 'graphics/photo5.jpg'),
-            ItemCard(
-                itemName: "Testi 6",
-                price: "200,00€",
-                image: 'graphics/photo1.jpg'),
-            ItemCard(
-                itemName: "Testi 7",
-                price: "300,00€",
-                image: 'graphics/photo2.jpg'),
-            ItemCard(
-                itemName: "Testi 8",
-                price: "200,00€",
-                image: 'graphics/photo3.jpg'),
-            ItemCard(
-                itemName: "Testi 9",
-                price: "300,00€",
-                image: 'graphics/photo4.jpg'),
-            ItemCard(
-                itemName: "Testi 10",
-                price: "200,00€",
-                image: 'graphics/photo5.jpg'),
-            ItemCard(
-                itemName: "Testi 11",
-                price: "300,00€",
-                image: 'graphics/photo1.jpg'),
-            ItemCard(
-                itemName: "Testi 12",
-                price: "200,00€",
-                image: 'graphics/photo2.jpg'),
-            ItemCard(
-                itemName: "Testi 13",
-                price: "300,00€",
-                image: 'graphics/photo3.jpg'),
-            ItemCard(
-                itemName: "Testi 14",
-                price: "200,00€",
-                image: 'graphics/photo4.jpg'),
-            ItemCard(
-                itemName: "Testi 15",
-                price: "100,00€",
-                image: 'graphics/photo5.jpg')
-          ],
-        ),
+      body: Column(
+        children: [Container(
+          color: Color.alphaBlend(Colors.black87, Colors.green),
+          height: 60,
+
+          child:
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+            TextButton(onPressed: () {}, child: const Text("Kategoria")),
+            TextButton(onPressed: () {}, child: const Text("Suodata")),
+            TextButton(onPressed: () {}, child: const Text("Näytä kartalla"))
+          ]),),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ItemCard(
+                      itemName: "Testi 1",
+                      price: "300,00€",
+                      image: 'graphics/photo1.jpg'),
+                  ItemCard(
+                      itemName: "Testi 2",
+                      price: "200,00€",
+                      image: 'graphics/photo2.jpg'),
+                  ItemCard(
+                      itemName: "Testi 3",
+                      price: "300,00€",
+                      image: 'graphics/photo3.jpg'),
+                  ItemCard(
+                      itemName: "Testi 4",
+                      price: "200,00€",
+                      image: 'graphics/photo4.jpg'),
+                  ItemCard(
+                      itemName: "Testi 5",
+                      price: "300,00€",
+                      image: 'graphics/photo5.jpg'),
+                  ItemCard(
+                      itemName: "Testi 6",
+                      price: "200,00€",
+                      image: 'graphics/photo1.jpg'),
+                  ItemCard(
+                      itemName: "Testi 7",
+                      price: "300,00€",
+                      image: 'graphics/photo2.jpg'),
+                  ItemCard(
+                      itemName: "Testi 8",
+                      price: "200,00€",
+                      image: 'graphics/photo3.jpg'),
+                  ItemCard(
+                      itemName: "Testi 9",
+                      price: "300,00€",
+                      image: 'graphics/photo4.jpg'),
+                  ItemCard(
+                      itemName: "Testi 10",
+                      price: "200,00€",
+                      image: 'graphics/photo5.jpg'),
+                  ItemCard(
+                      itemName: "Testi 11",
+                      price: "300,00€",
+                      image: 'graphics/photo1.jpg'),
+                  ItemCard(
+                      itemName: "Testi 12",
+                      price: "200,00€",
+                      image: 'graphics/photo2.jpg'),
+                  ItemCard(
+                      itemName: "Testi 13",
+                      price: "300,00€",
+                      image: 'graphics/photo3.jpg'),
+                  ItemCard(
+                      itemName: "Testi 14",
+                      price: "200,00€",
+                      image: 'graphics/photo4.jpg'),
+                  ItemCard(
+                      itemName: "Testi 15",
+                      price: "100,00€",
+                      image: 'graphics/photo5.jpg')
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black87,
@@ -171,30 +187,26 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               ElevatedButton.icon(
                 style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.green,
-          ),
+                  backgroundColor: Colors.green,
+                ),
                 onPressed: () {
-                  setState(() {
-
-                  });
+                  setState(() {});
                 },
-                icon: Icon(Icons.favorite_border,
+                icon: Icon(
+                  Icons.favorite_border,
                 ),
                 label: const Text('Omat suosikit'),
               ),
-
-        Container(
-          //alignment: AlignmentDirectional.bottomEnd,
-          child:
-              CircleAvatar(
-          radius: 25,
-          backgroundColor: Colors.white70,
-          child: IconButton(
-                onPressed: () {
-                },
-                icon: const Icon(Icons.add),
-          )
-              ),)
+              Container(
+                //alignment: AlignmentDirectional.bottomEnd,
+                child: CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white70,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add),
+                    )),
+              )
             ],
           ),
         ),
@@ -238,14 +250,11 @@ class ItemCard extends StatelessWidget {
                   radius: 30,
                   backgroundColor: Colors.green,
                   child: IconButton(
-                    icon: Icon(
-                      Icons.favorite_border,
-                      color: Colors.black,
-                    ),
-                      onPressed: () {
-
-                }
-                  ),
+                      icon: Icon(
+                        Icons.favorite_border,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {}),
                 ),
                 title: Text(itemName),
                 subtitle: Text(price),
